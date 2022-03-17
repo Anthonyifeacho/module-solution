b8e2c97d388812fd068e6ab7989f64390355cc48
Anthonyifeacho <anthonyifeacho@yahoo.com>
2022-03-17 17:31:16 +0000

    changes

--- module-4/SpeakGoodBye.js
+++ module-4/SpeakGoodBye.js
@@ -18,6 +18,7 @@
   // STEP 8: Rewrite the 'speak' function such that it is attached to the
   // byeSpeaker object instead of being a standalone function.
   // See Lecture 52, part 2
+  //added comment
   var byeSpeaker = function (name) {
     console.log(speakWord + " " + name);
   }
